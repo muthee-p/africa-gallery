10a6d8abc8fb594a9e0b0a231a7e688be2a721ce
Prudence Muthee <117924809+muthee-p@users.noreply.github.com>
2023-06-22 23:17:30 +0300

    Update JamesObonyo.js

--- src/pages/JamesObonyo.js
+++ src/pages/JamesObonyo.js
@@ -11,18 +11,14 @@ const JamesObonyo = () => {
 	<div className='w-[50%] p-8'>
 		<h3 className='text-center mb-4'>Whom am i? </h3>
 		<p>
-
-		Lorem ipsum dolor sit amet. Ea incidunt incidunt ut vitae temporibus aut asperiores quasi At possimus voluptatem. Vel aperiam voluptatum eum iusto molestiae aut illo officiis eos aspernatur exercitationem est veritatis fugiat eum magnam aspernatur. Ex accusantium harum qui molestiae corrupti et omnis molestiae vel voluptatibus inventore est incidunt dolore aut explicabo quas qui perferendis itaque. 
+			Hailing from the enchanting landscapes of Africa, James Obonyo is an extraordinary visionary who uses art as a medium to bridge cultures and ignite conversations. Rooted in their deep reverence for African traditions and the transformative power of art, James channels their experiences and emotions into mesmerizing creations that evoke a profound sense of introspection and connection. Through their masterful brushstrokes, [Artist's Name] captures the essence of Africa's soul, celebrating its diverse heritage and shedding light on its nuanced narratives.
+	  </p> <br />
+		<p>
+			With an intuitive understanding of color, texture, and composition, James crafts captivating artworks that are as visually stunning as they are thought-provoking. Their pieces often feature symbolic imagery, intertwining elements of nature, spirituality, and human experiences. Drawing inspiration from the resilience of African communities, James infuses their work with themes of identity, social justice, and the preservation of cultural legacy, using art as a catalyst for change and a vehicle for storytelling.
 		</p> <br />
 		<p>
-		Et earum explicabo et laborum blanditiis eum expedita obcaecati vel quia galisum non adipisci officia id assumenda accusamus.
-
-		Rem pariatur similique et similique tempora At dolores veniam non quia vitae. Aut adipisci consequuntur non dignissimos incidunt ex culpa aperiam est molestiae error ut repellendus voluptatem. Non consectetur animi aut minus placeat non maiores rerum. 
-		</p> <br />
-		<p> Eos aliquam repellat sit perspiciatis cupiditate est soluta dolorum qui aliquam enim ad alias voluptatem est laboriosam quaerat qui sunt quas.
-
-		Id quisquam quia eos dolorem doloribus ab porro ipsa et consequuntur animi rem voluptatem laboriosam non sint sunt. Sed ullam mollitia in provident iusto ut repellendus consequatur sit sapiente quaerat. Eum numquam laborum sed consequatur consequuntur et illum quaerat aut totam quas est quos omnis.
-		</p>
+	  		Recognized for their artistic contributions, James has exhibited their work in prestigious galleries and art institutions across the globe. Their art has captivated audiences with its profound depth and ability to evoke emotions, encouraging viewers to contemplate the complexities of the African experience. As James continues to evolve and push boundaries, their unwavering dedication to African artistry and their commitment to creating a dialogue between cultures ensures that their unique voice will resonate for generations to come, leaving an indelible mark on the global art scene.
+	  	</p>
 	</div>
 	</div>
 
@@ -30,7 +26,7 @@ const JamesObonyo = () => {
 	<h3 className='text-center mb-4'>ARTWORKS AVAILABLE</h3>
 	<div className='flex justify-around w-[100%] text-center'>
 		<div className='w-[50%] m-4 p-2'>
-			<Link to='/artists/jamesobonyo/jamesobonyoslide' className=' h-[36rem] flex justify-center mb-4'>
+			<Link to='/artwork/jamesobonyo/jamesobonyoslide' className=' h-[36rem] flex justify-center mb-4'>
 				<img src={AtusArt} alt="woman" />
 			</Link>
 			<h5>Id quisquam quia eos dolorem doloribus</h5>
