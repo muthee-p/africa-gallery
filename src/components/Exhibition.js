fe023db6e9ca21b003a93a0ba435d085a7628afb
Prudence Muthee <117924809+muthee-p@users.noreply.github.com>
2023-06-22 18:49:13 +0300

    Update Exhibition.js

--- src/components/Exhibition.js
+++ src/components/Exhibition.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BonMwiru from '../assets/bonmwiru.jpeg';
-
+import Mchechu from '../assets/mchechu.jpeg';
+import Kamnyoso from '../assets/kamnyoso.jpeg';
+import Kupoayo from '../assets/kupoayo3.jpeg';
 
 const Exhibition = () => {
   return (
@@ -15,20 +17,20 @@ const Exhibition = () => {
 		<h3 className='text-center m-4'>PAST EXHIBITIONS</h3>
 		<div className='flex flex-col items-center md:flex-row'>
 		<div className='md:w-1/3 md:p-2'>
-		<img src={BonMwiru} alt='man'className=''/>
-		<h5>moi Avenue</h5>
-		<p>November 2022</p>
+		<img src={Kamnyoso} alt='man'className=''/>
+		<h5>Mombasa, Kenya</h5>
+		<p>January 2022</p>
 		</div>
 		<div className='md:w-1/3 md:p-2' >
-		<img src={BonMwiru} alt='man'className='w'/>
-		<h5>moi Avenue</h5>
-		<p>November 2022</p>
+		<img src={Mchechu} alt='man'className='w'/>
+		<h5>Kenyatta Avenue, Tom's gallery</h5>
+		<p>May 2022</p>
 		</div>
 
 		<div className='md:w-1/3 md:p-2'>
-		<img src={BonMwiru} alt='man'className=''/>
-		<h5>moi Avenue</h5>
-		<p>November 2022</p>
+		<img src={Kupoayo} alt='man' className=''/>
+		<h5>Kisumu, Kenya</h5>
+		<p>February 2022</p>
 		</div>
 		</div>
 	</div>
