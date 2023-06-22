c43d51708f7f0e3a73f9b94948b11aa48526276d
Prudence Muthee <117924809+muthee-p@users.noreply.github.com>
2023-06-22 23:39:08 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,9 +71,9 @@ const Home = () => {
 		<h2>About Us</h2>
 		
 		<div className='md:w-[70%] p-4 flex items-center flex-col'>
-		<p className=''>
+		<p className='mb-3'>
 			Welcome to our African Gallery, where the vibrant and diverse artistry of Africa comes alive. Nestled in the heart of this rich continent, our gallery is a celebration of the extraordinary talent and cultural heritage of African artists. We believe in showcasing the unique narratives, traditions, and perspectives that emerge from Africa's artistic landscape.
-		</p><br>
+		</p>
 		<p>
 			Our collection features a captivating array of artwork from a myriad of African artists, each with their own distinctive style and voice. From awe-inspiring sculptures that pay homage to ancestral wisdom, to breathtaking paintings that explore the intricate tapestry of African life, every piece tells a story that resonates with the soul.
 			</p>
